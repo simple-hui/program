6b5ad5f43a65247fa8c812fe91f2cfdb1abc35c5
simple-hui <276557421@qq.com>
2018-07-03 11:22:30 +0800

    首页制作

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,13 +9,143 @@ Page({
     autoplay: true,
     interval: 5000,
     duration: 1000,
+    titleList: [
+      {
+        name: '榜单',
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      },
+      {
+        name: '听小说',
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      },
+      {
+        name: '免费领',
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      },
+      {
+        name: '听相声',
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      },
+      {
+        name: '听知识',
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      },
+    ],
     placeList: [
-      '在北京',
-      '秦皇岛',
-      '五台山',
-      '在拉萨',
-      '在珠峰',
-      '去哪里'
+      {
+        title: '免费领',
+        type: '0',
+        list:[
+          {
+            url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            title: '复旦女神教师陈果的幸福哲学课',
+            littleTitle: '认识自己 铭心见性',
+            need: '需要30人助力',
+            needPeople: '34人已领取',
+            prise: '99',
+          },
+          {
+            url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            title: '郦波品千古最美情诗',
+            littleTitle: '诗词大会嘉宾带你品味情诗之美',
+            need: '需要50人助力',
+            needPeople: '42人已领取',
+            prise: '199',
+          },
+          {
+            url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            title: '复旦女神教师陈果的幸福哲学课',
+            littleTitle: '认识自己 铭心见性',
+            need: '需要30人助力',
+            needPeople: '34人已领取',
+            prise: '99',
+          },
+          {
+            url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            title: '郦波品千古最美情诗',
+            littleTitle: '诗词大会嘉宾带你品味情诗之美',
+            need: '需要50人助力',
+            needPeople: '42人已领取',
+            prise: '199',
+          }
+        ]
+      },
+      {
+        title: '有声小说',
+        type: '1',
+        list: [
+          {
+            url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            title: '复旦女神教师陈果的幸福哲学课',
+            littleTitle: '认识自己 铭心见性认识自己 铭心见性认识自己 铭心见性',
+            need: '需要30人助力',
+            needPeople: '34人已领取',
+            prise: '99',
+          },
+          {
+            url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            title: '郦波品千古最美情诗',
+            littleTitle: '诗词大会嘉宾带你品味情诗之美',
+            need: '需要50人助力',
+            needPeople: '42人已领取',
+            prise: '199',
+          },
+          {
+            url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            title: '复旦女神教师陈果的幸福哲学课',
+            littleTitle: '认识自己 铭心见性',
+            need: '需要30人助力',
+            needPeople: '34人已领取',
+            prise: '99',
+          },
+          {
+            url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            title: '郦波品千古最美情诗',
+            littleTitle: '诗词大会嘉宾带你品味情诗之美',
+            need: '需要50人助力',
+            needPeople: '42人已领取',
+            prise: '199',
+          }
+        ]
+      },
+      {
+        title: '相声评书',
+        type: '1',
+        list: [
+          {
+            url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            title: '复旦女神教师陈果的幸福哲学课',
+            littleTitle: '认识自己 铭心见性认识自己 铭心见性认识自己 铭心见性',
+            need: '需要30人助力',
+            needPeople: '34人已领取',
+            prise: '99',
+          },
+          {
+            url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            title: '郦波品千古最美情诗',
+            littleTitle: '诗词大会嘉宾带你品味情诗之美',
+            need: '需要50人助力',
+            needPeople: '42人已领取',
+            prise: '199',
+          },
+          {
+            url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            title: '复旦女神教师陈果的幸福哲学课',
+            littleTitle: '认识自己 铭心见性',
+            need: '需要30人助力',
+            needPeople: '34人已领取',
+            prise: '99',
+          },
+          {
+            url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            title: '郦波品千古最美情诗',
+            littleTitle: '诗词大会嘉宾带你品味情诗之美',
+            need: '需要50人助力',
+            needPeople: '42人已领取',
+            prise: '199',
+          }
+        ]
+      }
     ]
   }
 })
